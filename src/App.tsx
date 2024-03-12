1d6aa16add69d1771d482cda27d95c8ac6783889
harjot <harjot.lotey@teachingstrategies.com>
2024-03-11 18:14:50 -0700

    Add tailwind | Add Basic page

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import Logo from "../public/HunarLogoJPG.jpg";
 function App() {
   return (
     <>
-      <img src={Logo} />
+      <div className="flex justify-center items-center">
+        <img src={Logo} />
+      </div>
       <div>
-        <i>Website coming soon</i>
+        <p className="text-slate-700"><i>Website coming soon</i></p>
+        <a href="https://www.instagram.com/hunarstudios_" className="text-yellow-700 hover:underline">Follow me on Instagram</a>
       </div>
     </>
   );
