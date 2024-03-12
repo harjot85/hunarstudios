ab0edc0c5a0222957322cc0c37fcc0a7ee27f816
harjot <harjot.lotey@teachingstrategies.com>
2024-03-11 17:45:04 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Logo from "../public/HunarLogoJPG.jpg";
+
+function App() {
+  return (
+    <>
+      <img src={Logo} />
+      <div>
+        <i>Website coming soon</i>
+      </div>
+    </>
+  );
+}
+
+export default App;
